682e85bba2d31216725f17b1c01f7594ed90534a
Mitya Kim <mitya_kim@icloud.com>
2021-11-09 14:39:20 -0800

    Final calc

--- calc.js
+++ calc.js
@@ -15,6 +15,23 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+  } else if (mathSymbol === "/"){
+	    console.log(num1 / num2);
+
+  } else if (mathSymbol === "-"){
+	console.log(num1 - num2);
+
+  } else if (mathSymbol === "*"){
+ 	console.log(num1 * num2);
+
+  } else if (mathSymbol === "sqrt"){
+	console.log(Math.sqrt(num1));
+  }
+
+    console.log(num1,mathSymbol, num2)
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
